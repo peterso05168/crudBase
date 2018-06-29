a6ef29ddff6094c2c7fd35acaddac79310923297
pso <peterso05168@gmail.com>
2018-06-29 16:20:51 +0800

    changed build path

--- src/main/java/app/EasyNotesApplication.java
+++ src/main/java/app/EasyNotesApplication.java
@@ -2,11 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EasyNotesApplication {
+public class EasyNotesApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EasyNotesApplication.class, args);
